ae6a38f9c72175d60fa3bfd4d3f0369c2fbbf64e
krlove <mischenkoandrey@gmail.com>
2020-07-25 18:55:42 +0200

    Lobby design

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -2,13 +2,20 @@
     <div>
         <div class="box px-6 py-6">
             <div class="columns">
-                <div class="column is-one-quarter">
+                <div class="column is-one-third">
                     <div class="message is-dark">
                         <div class="message-header">
-                            Players online (2)
+                            Players online
                         </div>
-                        <div class="message-body has-background-white">
-                            Players list
+                        <div class="players-list-body message-body has-background-white">
+                        </div>
+                    </div>
+
+                    <div class="message is-dark">
+                        <div class="message-header">
+                            Games
+                        </div>
+                        <div class="games-body message-body has-background-white">
                         </div>
                     </div>
                 </div>
@@ -17,18 +24,12 @@
                         <div class="message-header">
                             Chat
                         </div>
-                        <div class="message-body has-background-white">
-                            Chat
-                        </div>
-                    </div>
-                </div>
-                <div class="column is-one-quarter">
-                    <div class="message is-dark">
-                        <div class="message-header">
-                            Games
-                        </div>
-                        <div class="message-body has-background-white">
-                            Games
+                        <div class="chat-body message-body has-background-white">
+                            <div class="field">
+                                <input class="input" type="text" />
+                            </div>
+                            <div class="message-list">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -46,5 +47,29 @@
 </script>
 
 <style scoped>
+    .players-list-body {
+        height: 400px;
+        overflow: auto;
+    }
 
+    .games-body {
+        height: 200px;
+        overflow: auto;
+    }
+
+    .chat-body {
+        height: 662px;
+        display: flex;
+        flex-flow: column-reverse nowrap;
+    }
+
+    .message-list {
+        display: flex;
+        flex-flow: column-reverse nowrap;
+        overflow: auto;
+        height: 100%;
+        border: 1px solid lightgrey;
+        margin: 10px 0;
+        padding: 5px
+    }
 </style>
