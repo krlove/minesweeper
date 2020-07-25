81dc7f14116150c754ea7cf96a70429420e9301f
krlove <mischenkoandrey@gmail.com>
2020-07-25 16:33:37 +0200

    Numbers

--- src/components/Lives.vue
+++ src/components/Lives.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <span
-                v-for="n in total"
+                v-for="n of range"
                 v-bind:key="n"
                 class="icon"
                 v-bind:class="{ 'cursor-pointer': selectable, 'disabled': disabled }"
@@ -18,12 +18,22 @@
 <script lang="ts">
     import {Component, Prop, Vue} from "vue-property-decorator";
 
+    function range(start: number, end: number): number[] {
+        return Array.from({ length: end - start + 1 }, (_, i) => i + 1)
+    }
+
     @Component
     export default class Lives extends Vue {
         @Prop() total: number;
         @Prop() selectable: boolean;
         @Prop() value: number;
         @Prop() disabled: boolean;
+
+        get range(): number[] {
+            const r = range(1, this.total);
+
+            return r;
+        }
 
         onStarClick(n: number): void {
             if (!this.selectable || this.disabled) {
